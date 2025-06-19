5be3e973e3b1bc747fb5d90fc465fd6ef3b8ee16
abhi8127 <abhisheky06880@gmail.com>
2025-06-19 16:46:58 +0530

    Initial commit for spark4.0 project

---
+++ editplus/RelOperation.java
@@ -0,0 +1,12 @@
+class RelOperation 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		int a=10,b=20;
+		System.out.println(a==b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		
+	}
+}
