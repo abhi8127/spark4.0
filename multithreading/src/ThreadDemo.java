660edd6a700bb4c11f9daf7e239ed6f19a4f032e
abhi8127 <abhisheky06880@gmail.com>
2025-06-20 19:56:57 +0530

    Thread Demo

--- multithreading/src/ThreadDemo.java
+++ multithreading/src/ThreadDemo.java
@@ -1,5 +1,23 @@
+class MyThread extends Thread{
+    @Override
+    public void run() {
+        for(int i=0;i<5;i++)
+        {
+            System.out.println("child Thread");
+        }
+    }
+}
+
 public class ThreadDemo {
     public static void main(String[] args) {
+
+        MyThread t1= new MyThread();
+        //t1.start(); the run() method would execute on the main thread, not in a new thread.
+        t1.run();
+        for(int i=0;i<6;i++)
+        {
+            System.out.println("Main Thread");
+        }
 
 
     }
