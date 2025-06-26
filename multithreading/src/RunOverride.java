b98e6b43d60cadd61769206849b3d648d9b73b55
abhi8127 <abhisheky06880@gmail.com>
2025-06-26 11:54:18 +0530

    ..

--- multithreading/src/RunOverride.java
+++ multithreading/src/RunOverride.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+class MyRunThread extends Thread{
+
+}
 
 public class RunOverride {
+    public static void main(String[] args) {
+   MyRunThread t=new MyRunThread();
+   t.start();
+   for(int i=0;i<5;i++)
+   {
+       System.out.println("main");
+   }
+    }
 }
