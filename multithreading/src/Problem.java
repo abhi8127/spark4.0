d676fdd9c5f82977782bf1eecaf4edfcdee02ada
abhi8127 <abhisheky06880@gmail.com>
2025-06-26 12:11:49 +0530

    Problem with multithreading(race condition) that why we use synchronisation

--- multithreading/src/Problem.java
+++ multithreading/src/Problem.java
@@ -1,2 +1,33 @@
+class MyThread3 extends Thread{
+    int sum=0;
+
+    @Override
+    public void run() {
+        for(int i=0;i<10;i++)
+        {
+            sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+}
+
 public class Problem {
+    public static void main(String[] args) {
+      /* MyThread3 t1=new MyThread3();   // both object are different so there is no chance of race condition
+        MyThread3 t2=new MyThread3();
+        t1.start();
+        t2.start();
+       */
+
+        MyThread3 obj = new MyThread3();       //  Shared object
+        Thread t1 = new Thread(obj);           // Thread-1 using same object
+        Thread t2 = new Thread(obj);           //  Thread-2 using same object
+        t1.start();
+        t2.start();
+
+        /*
+        so in this there is race condition
+         */
+
+    }
 }
