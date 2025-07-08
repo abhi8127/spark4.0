4cd4781422b0b887ccac9d2b34307eaa610a0ecf
abhi8127 <abhisheky06880@gmail.com>
2025-07-08 17:08:34 +0530

    JDBC

--- JDBCP01/src/JDBCDemo.java
+++ JDBCP01/src/JDBCDemo.java
@@ -6,12 +6,20 @@ import java.sql.Statement;
 public class JDBCDemo {
     public static void main(String[] args) {
         try {
+            // Load MySQL JDBC Driver
             Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Establish a connection to the database (URL, username, password)
             Connection con=DriverManager.getConnection(
                     "jdbc:mysql://127.0.0.1:3306/mystd", "root","Abhishek@123");
 
+            // Create a Statement object to execute SQL queries
             Statement statement= con.createStatement();
+
+            // Define SQL query to fetch all records from 'student' table
             String query="select * from student";
+
+            // Execute the SQL query and store the result in ResultSet
             ResultSet rs=statement.executeQuery(query);
 
             System.out.println(" ---------------Read Data------------ ");
