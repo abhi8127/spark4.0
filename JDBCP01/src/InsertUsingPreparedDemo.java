0a4ee6f7739ed0683e7034c7f53c89ab2ee06ed2
abhi8127 <abhisheky06880@gmail.com>
2025-07-10 11:47:44 +0530

    inserting one data into mysql database using PreparedStatement

--- JDBCP01/src/InsertUsingPreparedDemo.java
+++ JDBCP01/src/InsertUsingPreparedDemo.java
@@ -5,18 +5,26 @@ import java.sql.PreparedStatement;
 public class InsertUsingPreparedDemo {
     public static void main(String[] args) {
         try{
+
+            // Load MySQL JDBC driver
             Class.forName("com.mysql.cj.jdbc.Driver");
 
+            // Establish connection to MySQL DB 'mystd'
             Connection con= DriverManager.getConnection(
                     "jdbc:mysql://127.0.0.1:3306/mystd","root","Abhishek@123");
 
+            // Define parameterized SQL query with placeholders (?)
             String query="insert into student (id,stdName,age) values (?,?,?)";
 
+            // Create PreparedStatement object
             PreparedStatement pstmt=con.prepareStatement(query);
-            pstmt.setInt(1,8);
-            pstmt.setString(2,"shivansh");
-            pstmt.setInt(3,12);
 
+            // Set values for the placeholders (1-based indexing)
+            pstmt.setInt(1, 9);             // Set id = 8
+            pstmt.setString(2, "shiva"); // Set stdName = "shivansh"
+            pstmt.setInt(3, 28);            // Set age = 12
+
+            // Execute the update query and get number of affected rows
             int update= pstmt.executeUpdate();
             System.out.println(update+" Rows "+"Updated");
             con.close();
