aca36d32f76d12fd85483329fcdec56dd27e038d
abhi8127 <abhisheky06880@gmail.com>
2025-07-30 14:19:23 +0530

    Used of enumeration(ligacy interface)for traverse over collection like vector,HashTable

--- collection/src/vectorDemo/VectorDemo.java
+++ collection/src/vectorDemo/VectorDemo.java
@@ -1,5 +1,6 @@
 package vectorDemo;
 
+import java.util.Enumeration;
 import java.util.Vector;
 
 public class VectorDemo {
@@ -18,6 +19,20 @@ public class VectorDemo {
         }
         else {
             System.out.println("Not empty");
+        }
+
+        /*
+        Enumeration is a legacy interface used for iterating over older collections
+        like Vector and Hashtable.
+        It was introduced before Iterator and is part of the java.util package.
+
+        and the drawback of this is it only fetch element not remove element from colletion
+
+         */
+        Enumeration e= vector.elements();
+        while (e.hasMoreElements())
+        {
+            System.out.println(e.nextElement());
         }
     }
 }
